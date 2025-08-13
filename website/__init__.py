684ef428ba8cda7290bfd6adfb19a38e0ee4085c
zayd-mzn <zaydmeziane123@gmail.com>
2025-08-14 00:47:33 +0100

    commit

--- website/__init__.py
+++ website/__init__.py
@@ -33,60 +33,6 @@ def create_app():
         if Lesson.query.count() == 0:
             lesson = Lesson(
                 title="Introduction to Python",
-                content="""Welcome to the first lesson! Here we learn about Python basics.
-1. What is Python?
-Python is a high-level, interpreted programming language known for its simplicity -its syntax is close to plain English-, 
-versatility –used in web development, data science, AI, automation,..., community support –many libraries and frameworks to
-help you build faster.
-
-2. Why Learn Python?
-Easy to learn for beginners. Powerful enough for professionals. Cross-platform (works on Windows, macOS, Linux).
-Huge job market demand.
-
-3. Your First Python Program
-In Python, printing text is as simple as:
-
-print("Hello, world!")
-
-How it works:
-print() is a built-in function that displays output on the screen. Text is written inside quotes " ".
-
-4. Python Basics
-Variables : Variables store information in memory.
-
-name = "Alice"
-age = 20
-
-Name is a string ("Alice"). Age is an integer (20).
-
-Data Types : Common Python data types:
-str → "Hello"
-int → 42
-float → 3.14
-bool → True / False
-
-5. Comments
-Comments are ignored by Python and used to explain code.
-
-# This is a comment
-
-6. Basic Math in Python
-
-x = 10
-y = 3
-print(x + y)  # Addition → 13
-print(x - y)  # Subtraction → 7
-print(x * y)  # Multiplication → 30
-print(x / y)  # Division → 3.333...
-print(x // y) # Floor division → 3
-print(x % y)  # Modulus → 1
-
-7. Next Steps
-After learning the basics, you’ll explore:
-Conditionals (if, else)
-Loops (for, while)
-Functions
-Modules and Libraries...""",
                 category="Programming"
             )
             db.session.add(lesson)
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Lesson
+from .models import Lesson, UserProgress
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -9,8 +10,31 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+@views.route("/lessons")
+@login_required
+def lessons():
+    lessons = Lesson.query.all()
+    user_progress = UserProgress.query.filter_by(user_id=current_user.id).all()
+    progress_dict = {p.lesson_id: p.completed for p in user_progress}
+
+    return render_template(
+        "lessons.html",
+        lessons=lessons,
+        progress=progress_dict,
+        user=current_user
+    )
+
 @views.route('/lesson')
 @login_required
 def lesson():
     lesson = Lesson.query.first()
-    return render_template('lesson.html', lesson=lesson, user=current_user)
+    progress = UserProgress.query.filter_by(user_id=current_user.id,lesson_id=1).first()
+    return render_template('lesson.html', lesson=lesson, user=current_user, progress=progress)
+
+@views.route('/lesson2')
+@login_required
+def lesson2():
+    progress = UserProgress.query.filter_by(user_id=current_user.id, lesson_id=2).first()
+    
+    return render_template('lesson2.html', user=current_user, progress=progress)
+
